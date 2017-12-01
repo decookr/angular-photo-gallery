bbe99872bc88c64ca80a75e8eabb471cbbcc5371
Ryan DeCook <ryandecook@gmail.com>
2017-12-01 17:59:37 -0600

    button clicks display on dom

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,16 +1,19 @@
 var app = angular.module('PhotosApp', []);
-
 app.controller('PhotoController', function (){
     console.log('PhotoController has been loaded');
     var self = this;
     self.message = 'Hello world!!';
-
+    var clicks = 1;
+    
     self.photosArray =[ 
         {
            name: 'Family',
            url: '/images/family.jpg',
-           count: 0,
         }
     ];
-    
+
+    self.addClicks=function(){
+        self.buttonClicks = clicks++;
+       console.log('clicks', clicks);
+    }
 });
